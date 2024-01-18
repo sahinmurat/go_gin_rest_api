b9e13db9a099523a583a5113b26471edf1d7af41
Murat Sahin <murat.sahin@inevvo-solutions.com>
2024-01-18 11:07:56 +0100

    get one article endpoint is added

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
@@ -17,13 +18,27 @@ var articles = []Article{
 }
 
 func getAllArticles(c *gin.Context) {
+	fmt.Println("all article")
 	c.JSON(http.StatusOK, articles)
+}
+
+func getOneArticle(c *gin.Context) {
+	fmt.Println("one article")
+	id := c.Param("id")
+	for _, article := range articles {
+		if article.ID == id {
+			c.JSON(http.StatusOK, article)
+			return
+		}
+	}
+	c.JSON(http.StatusNotFound, gin.H{"error": "Not Found"})
 }
 
 func main() {
 	router := gin.Default()
 
 	router.GET("/articles", getAllArticles)
+	router.GET("/articles/:id", getOneArticle)
 
 	router.Run("localhost:8080")
 }
